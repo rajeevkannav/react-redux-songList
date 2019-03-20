cd3af44256ed778cf9a908a9485e2d1f00eb8ebf
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-20 12:01:56 +0530

    Updates

---
+++ precache-manifest.d7ba2bc414a7a13cc9fb3a65b281ce48.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c8eddf3a7de97c1609d",
+    "url": "/react-redux-songList/static/js/runtime~main.77626357.js"
+  },
+  {
+    "revision": "5d702e6d96a3806d5807",
+    "url": "/react-redux-songList/static/js/main.31ec910b.chunk.js"
+  },
+  {
+    "revision": "59012b6868dd6ff110a5",
+    "url": "/react-redux-songList/static/js/2.baa79972.chunk.js"
+  },
+  {
+    "revision": "6a5698c058de28cac37387a9be20882b",
+    "url": "/react-redux-songList/index.html"
+  }
+];
